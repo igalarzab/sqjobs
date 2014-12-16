d8f1f733675e7e868a46d181b49e51da25a99eec
Jose Ignacio Galarza <igalarzab@gmail.com>
2014-12-17 00:13:47 +0100

    Fix error setting the right queue

--- sqjobs/worker.py
+++ sqjobs/worker.py
@@ -42,6 +42,7 @@ class Worker(object):
         job = job_class()
 
         job.id = payload['_metadata']['id']
+        job.queue = self.queue_name
         job.retries = payload['_metadata']['retries']
         job.created_on = payload['_metadata']['created_on']
         job.first_execution_on = payload['_metadata']['first_execution_on']
