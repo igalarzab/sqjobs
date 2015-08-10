f289699b35b5b7698cf3b484e7157b3df0cbf88d
Esteban Sánchez <esteban.sanchez@gmail.com>
2015-08-10 15:06:37 +0200

    Improved worker and broker tests, increased worker coverage to 100%

--- sqjobs/tests/fixtures.py
+++ sqjobs/tests/fixtures.py
@@ -4,6 +4,7 @@ from ..job import Job
 class Adder(Job):
     name = 'adder'
     queue = 'default'
+    # We set this time so it's re-queued in Dummy connector
     retry_time = 10
 
     def run(self, num1, num2):
@@ -26,6 +27,62 @@ class Divider(Job):
 
     def on_failure(self, *args, **kwargs):
         self.err = 'ZeroDivisionError'
+
+
+class RetryJob(Job):
+    name = 'retry'
+    retried_callback_called = False
+
+    @classmethod
+    def has_retried(cls):
+        return cls.retried_callback_called
+
+    @classmethod
+    def retried(cls):
+        cls.retried_callback_called = True
+
+    @classmethod
+    def reset_retried(cls):
+        cls.retried_callback_called = True
+
+    def __init__(self):
+        super(RetryJob, self).__init__()
+        RetryJob.reset_retried()
+
+    def run(self):
+        self.retry()
+
+    def on_retry(self):
+        RetryJob.retried()
+
+
+class ExceptionJob(Job):
+    name = 'exception'
+    failed_callback_called = False
+    # We set this time so it's re-queued in Dummy connector
+    retry_time = 10
+
+    @classmethod
+    def has_failed(cls):
+        return cls.failed_callback_called
+
+    @classmethod
+    def failed(cls):
+        cls.failed_callback_called = True
+
+    @classmethod
+    def reset_failed(cls):
+        cls.failed_callback_called = True
+
+    def __init__(self):
+        super(ExceptionJob, self).__init__()
+        ExceptionJob.reset_failed()
+
+    def run(self):
+        raise Exception("Test")
+
+    def on_fail(self):
+        ExceptionJob.retried()
 
 
 class FakeAdder(Adder):
