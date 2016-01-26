9341cdab4ac92c58103514189ccf3cfa3846bd50
Jose Ignacio Galarza <igalarzab@gmail.com>
2016-01-26 17:49:09 +0100

    Deleting beat for now... it must be independent from django

--- sqjobs/contrib/django/djsqjobs/management/commands/sqjobs.py
+++ sqjobs/contrib/django/djsqjobs/management/commands/sqjobs.py
@@ -3,10 +3,8 @@ from __future__ import absolute_import
 from django.conf import settings
 from django.core.management.base import BaseCommand
 
-from sqjobs import create_sqs_worker, create_sqs_broker, create_eager_broker
+from sqjobs import create_sqs_worker
 from sqjobs.contrib.django.djsqjobs.utils import register_all_jobs
-
-from sqjobs.contrib.django.djsqjobs.beat import Beat
 
 
 class Command(BaseCommand):
@@ -21,13 +19,6 @@ class Command(BaseCommand):
 
             self._execute_worker(args[1])
 
-        elif args[0] == 'beat':
-            if len(args) not in (2, 3):
-                self.help_text()
-                return
-
-            self._execute_beat(*args[1:])
-
     def _execute_worker(self, queue_name):
         worker = create_sqs_worker(
             queue_name=queue_name,
@@ -40,18 +31,6 @@ class Command(BaseCommand):
         register_all_jobs(worker)
         worker.execute()
 
-    def _execute_beat(self, sleep_interval, skip_jobs=True):
-        broker = create_sqs_broker(
-            access_key=settings.SQJOBS_SQS_ACCESS_KEY,
-            secret_key=settings.SQJOBS_SQS_SECRET_KEY,
-            region=settings.SQJOBS_SQS_REGION,
-            use_ssl=getattr(settings, 'SQJOBS_SQS_USE_SSL', True),
-        )
-        beat = Beat(broker, int(sleep_interval), skip_jobs)
-        register_all_jobs(beat)
-        beat.run_forever()
-
     def help_text(self):
         self.stdout.write('Use:')
         self.stdout.write('./manage.py sqjobs worker QUEUE_NAME')
-        self.stdout.write('./manage.py sqjobs beat SLEEP_INTERVAL [SKIP_JOBS]')
