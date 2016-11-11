e0ae145d0fdfddb81fdb4d6f2aac60dd3511b555
Carlos Edo <carlosedo14@gmail.com>
2016-11-11 16:46:49 -0500

    add rabbitmq broker and worker to __init__.py

--- sqjobs/__init__.py
+++ sqjobs/__init__.py
@@ -1,6 +1,7 @@
 from .exceptions import RetryException
 from .job import Job
-from .utils import create_eager_broker, create_sqs_broker, create_sqs_worker
+from .utils import (create_eager_broker, create_sqs_broker, create_sqs_worker,
+    create_rabbitmq_broker, create_rabbitmq_worker)
 
 
 __all__ = [
@@ -8,5 +9,7 @@ __all__ = [
     'RetryException',
     'create_sqs_broker',
     'create_sqs_worker',
+    'create_rabbitmq_broker',
+    'create_rabbitmq_worker',
     'create_eager_broker'
 ]
