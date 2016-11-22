7ea540940d17055c15cef943749b03bde0f20de2
Carlos Edo <carlosedo14@gmail.com>
2016-11-22 14:58:11 -0500

    timeout should always be 0 for this mode to work

--- sqjobs/brokers/multiqueue.py
+++ sqjobs/brokers/multiqueue.py
@@ -8,11 +8,11 @@ class MultiQueue(Standard):
     """
     Broker to execute jobs in an asynchronous way from multiple queues
     """
-    def jobs(self, queue_names, timeout=20):
+    def jobs(self, queue_names, timeout=0):
         for queue_name in cycle(queue_names):
             while True:
-                payload = self.connector.dequeue(queue_name, wait_time=timeout)
-                if payload or not timeout:
+                payload = self.connector.dequeue(queue_name, wait_time=0)
+                if payload:
                     yield payload
                 else:
                     break
